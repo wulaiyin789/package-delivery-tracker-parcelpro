58ab00475e94df84653a0e70c7c7740b8132d435
Peter Wu <peterwlyau@gmail.com>
2026-03-31 13:38:31 +1000

    PDT-78 - test jira key for issue tracking

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -10,7 +10,7 @@ const router = express.Router();
 
 //TODO Add User Create Route (Admin only) in the future
 
-// GET /api/users/search?q=
+// GET /api/users/search?q= 
 router.get('/search', protect, authorize('ADMIN'), searchUsers);
 
 // PUT /api/users/activate/:id
@@ -22,7 +22,7 @@ router.put('/deactivate/:id', protect, authorize('ADMIN'), deactivateUser);
 // DELETE /api/users/:id
 router.delete('/:id', protect, authorize('ADMIN'), deleteUser);
 
-// PUT /api/users/:id/role
+// PUT /api/users/:id/role (PDT-78)
 router.put('/:id/role', protect, authorize('ADMIN'), updateRole);
 
 module.exports = router;
