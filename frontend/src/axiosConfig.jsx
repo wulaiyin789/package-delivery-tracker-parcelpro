223ea6dc365a49ad54e22efeea9e5ae4434fe5cc
Peter Wu <peterwlyau@gmail.com>
2026-04-03 22:07:52 +1000

    Add automated for frontend
    Instead of hardcode base url variable, put the base url to .env

--- frontend/src/axiosConfig.jsx
+++ frontend/src/axiosConfig.jsx
@@ -1,8 +1,8 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-  baseURL: 'http://localhost:5001', // local
-  //baseURL: 'http://13.55.24.8:5001', // live
+  baseURL: process.env.REACT_APP_BASE_URL ? process.env.REACT_APP_BASE_URL : 'http://localhost:5001', // local
+  // baseURL: 'http://13.55.24.8:5001', // live
   headers: { 'Content-Type': 'application/json' }
 });
 
